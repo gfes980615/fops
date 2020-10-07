398d07b1a4c6aae475ab8e9527fd5311e33a35b1
fred_chen <ltscyt0717@gmail.com>
2020-10-08 00:26:57 +0800

    import cobra pakage

---
+++ fops.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+var (
+	fileName string
+)
+
+func init() {
+	flag.StringVar(&fileName, "f", "", "the file")
+	//flag.Usage = usage
+}
+
+// 印出預設的說明
+//func usage() {
+//	fmt.Fprintf(os.Stderr, "Usage: math [options] [root]\n")
+//	fmt.Fprintf(os.Stderr, "  Currently, there are four URI routes could be used:\n")
+//	flag.PrintDefaults()
+//}
+
+func main() {
+	flag.Parse()
+	fmt.Println(flag.Args())
+	b, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Print(err)
+	}
+	str := string(b)
+	fmt.Println(str)
+	fmt.Println(fileLineCount(str))
+}
+
+func fileLineCount(text string) int {
+	return len(strings.Split(text, "\n"))
+}
