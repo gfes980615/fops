dd56952d6e94e66146dda591c7dcfb79b9639da8
fred_chen <ltscyt0717@gmail.com>
2020-11-01 23:27:52 +0800

    新增tourist_attraction_list

--- sql/base/init_diana.sql
+++ sql/base/init_diana.sql
@@ -44,3 +44,15 @@ CREATE TABLE `kktix_activity` (
   `activity_time` text CHARSET utf8mb4,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `tourist_attraction_list` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `url` varchar(200) NOT NULL,
+   `place` varchar(45) DEFAULT NULL,
+   `country` varchar(45) DEFAULT NULL,
+   `location` varchar(45) DEFAULT NULL,
+   `address` varchar(45) DEFAULT NULL,
+   `activity_time` varchar(200) DEFAULT NULL,
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `url_UNIQUE` (`url`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
